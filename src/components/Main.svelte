f59b701bfb256206be00f232cfe8a88cdb78d87c
Marble <scnideffer@gmail.com>
2024-01-07 15:10:24 -0800

    Fixed fontawesome stylesheet link - icons now properly displaying continuing to work on candidate chart

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -152,11 +152,38 @@
           <div class="flex flex-col gap-6 sm:gap-8">
             <h3 class="text-2xl sm:text-3xl md:text-5xl">
               {benefit.name}
-              <p>{benefit.description}</p>
             </h3>
+            <p>{benefit.description}</p>
           </div>
         </div>
       {/each}
+    </div>
+    <h5>The <span class="text-violet-400">Complete</span> Package</h5>
+    <div
+      class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+    >
+      <table class="bg-white text-slate-700 rounded text-center">
+        <thead class="border-b border-solid border-slate-200">
+          <tr class="">
+            <th />
+            <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+            <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+            <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8"
+              >Me</th
+            >
+          </tr>
+        </thead>
+        <tbody class="">
+          <tr class="border-b border-solid border-slate-200">
+            <td
+              class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+              >Dedication</td
+            >
+            <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </main>
