01fc6afd7fd62aaded1e3575b58fcea74f9caf7f
Marble <scnideffer@gmail.com>
2024-01-07 18:05:45 -0800

    maybe github pages workes as intended now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import "../app.css";
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
+  export const prerender = true;
 
   let y;
   let innerHeight = 0;
